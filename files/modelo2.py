fb6eb26f3e4098f8d755fd590e573b8ad718006e
Daiana <daianadte@gmail.com>
2022-04-30 09:44:03 -0300

    update modelo2

--- files/modelo2.py
+++ files/modelo2.py
@@ -11,9 +11,16 @@ import numpy as np
 
 
 def Top_products (df_views, advertiser_id):
+    #Leo los datos de las views
     df = pd.read_csv()
+    #Filtro por la fecha de hoy
     df_today = df =  df[df['date'] == datetime.datetime.now().date()]
+    #Cuento las views por producto-advertiser
     df_today = pd.DataFrame({'count' : df.groupby(['advertiser_id', 'product_id'])['product_id'].count().sort_values(ascending=False)}).reset_index()
+    #Devuelvo los 20 productos mas vistos por advertiser
+    df_top20 = df_today.groupby('advertiser_id').apply(lambda x : x.sort_values(by = 'count', ascending = False).head(20).reset_index(drop = True)).droplevel(level=0)
+
+    return df_top20
 
 
 
