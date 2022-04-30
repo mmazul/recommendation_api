b997a8eb19b47582c2b2448987428c652e3a2ac5
daiana duarte <daianadte@gmail.com>
2022-04-30 14:29:37 -0300

    update modelo 2 with s3 path

--- files/modelo2.py
+++ files/modelo2.py
@@ -10,10 +10,10 @@ import datetime
 import numpy as np
 
 
-def Top_products (df_views, advertiser_id):
+def Top_products ():
     #Leo los datos de las views
-    df = pd.read_csv()
-    #Filtro por la fecha de hoy
+    df = pd.read_csv('s3://recommendation-api-morales/product_views.csv')
+    #Filtro por la fecha de hoyclea
     df_today = df =  df[df['date'] == datetime.datetime.now().date()]
     #Cuento las views por producto-advertiser
     df_today = pd.DataFrame({'count' : df.groupby(['advertiser_id', 'product_id'])['product_id'].count().sort_values(ascending=False)}).reset_index()
@@ -21,6 +21,7 @@ def Top_products (df_views, advertiser_id):
     df_top20 = df_today.groupby('advertiser_id').apply(lambda x : x.sort_values(by = 'count', ascending = False).head(20).reset_index(drop = True)).droplevel(level=0)
 
     return df_top20
+
 
 
 
