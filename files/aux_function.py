5d6cfcfbddff512921c96b7ae1d53e8d0b2256df
Micaela Morales <micaelamorales@NB-ZXMD6P.local>
2022-04-30 14:31:55 -0300

    changes s3 connection

--- files/aux_function.py
+++ files/aux_function.py
@@ -1,12 +1,17 @@
 """ Auxiliar functions """
 
-import boto3
+import pandas as pd
 
-def upload_pandas_to_s3(dataframe, name: str):
-    dataframe.to_csv(name)
-    s3 = boto3.client("s3")
-    s3.upload_file(
-        Filename=name,
-        Bucket="recommendation-api-morales",
-        Key=name,
-    )
+def upload_pandas_to_s3(dataframe,
+                        name: str,
+                        bucket: str = 'recommendation-api-morales',
+                        extension: str = 'csv'):
+
+    dataframe.to_csv(f's3://{bucket}/{name}.{extension}', index=False)
+
+def download_s3_to_pandas(name: str,
+                          bucket: str = 'recommendation-api-morales',
+                          extension: str = 'csv') -> pd.DataFrame:
+
+    df = pd.read_csv(f's3://{bucket}/{name}.{extension}', index=False)
+    return df
