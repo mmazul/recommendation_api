82e74af59aa9e21902ceffdcb9b6f0049f0b759a
Micaela Morales <micaelamorales@NB-ZXMD6P.local>
2022-05-01 19:17:52 -0300

    change to airflow

--- dags/dag_args.py
+++ dags/dag_args.py
@@ -7,8 +7,7 @@ from datetime import timedelta
 default_args = {
     'owner': 'team-hackers-udesa',
     'start_date': days_ago(7),
-    'email': ['micaelamorales90@gmail.com', 'geronimodesimoni@gmail.com',
-              'daianadt@gmail.com'],
+    'email': ['teamhackersudesa@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 3,
