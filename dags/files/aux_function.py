078804c77bc9f04815fd92b3ba01a4f5f7d2db8d
Micaela Morales <micaelamorales@NB-ZXMD6P.local>
2022-05-11 23:59:46 -0300

    error in download function

--- dags/files/aux_function.py
+++ dags/files/aux_function.py
@@ -13,5 +13,5 @@ def download_s3_to_pandas(name: str,
                           bucket: str = 'recommendation-api-morales',
                           extension: str = 'csv') -> pd.DataFrame:
 
-    df = pd.read_csv(f's3://{bucket}/{name}.{extension}', index=False)
+    df = pd.read_csv(f's3://{bucket}/{name}.{extension}')
     return df
